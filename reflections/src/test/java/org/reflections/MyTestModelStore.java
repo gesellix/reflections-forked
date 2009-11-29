f6e7a55b3647728f8c52b1247c7aad50c0e78a48
Ron M <ronmamo@gmail.com>
2009-11-29 10:00:59 +0000

    +javadoc with svn mime-type prop set
    git-svn-id: http://reflections.googlecode.com/svn/trunk@36 71d6c4fe-fe51-0410-8f60-b1193cea1bb0

--- reflections/src/test/java/org/reflections/MyTestModelStore.java
+++ reflections/src/test/java/org/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Sat Nov 28 20:32:47 IST 2009]
+//generated using Reflections JavaCodeSerializer [Sat Nov 28 21:28:30 IST 2009]
 package org.reflections;
 
 import static org.reflections.serializers.JavaCodeSerializer.*;
